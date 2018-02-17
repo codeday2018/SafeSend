c644747dfa0270dcf79419c24aaa3421dc1489d4
codeday2018 <36574010+codeday2018@users.noreply.github.com>
2018-02-17 16:32:52 -0600

    Add files via upload

--- app/src/main/java/android/dev/safesend/MainActivity.java
+++ app/src/main/java/android/dev/safesend/MainActivity.java
@@ -5,17 +5,27 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    Button sendBtn;
+    EditText txtphoneNo;
+    EditText txtMessage;
+    String phoneNo;
+    String message;
 
-    @Override
+  /*  @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -38,6 +48,33 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, groupsArrayList);
 
         listView.setAdapter(adapter);
+    }  */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+          sendBtn = (Button) findViewById(R.id.buttonSend);
+     //  txtphoneNo = (EditText) findViewById(R.id.editText);
+     //  txtMessage = (EditText) findViewById(R.id.editText2);
+        System.out.print("gonna kms");
+        Log.d("gonna kms","gonna kms");
+
+       sendBtn.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View view) {
+               Text sender = new Text(new Contact("Riya","6159727656"));
+               try {
+                   Log.d("kms2","kms2");
+                   Toast.makeText(getApplicationContext(),"WIN",Toast.LENGTH_LONG).show();
+
+                   sender.sendSMS();
+               }catch(Exception e){
+                   Log.d("kms3","kms3");
+                   System.out.print("FAIL");
+                   Toast.makeText(getApplicationContext(),"FAIL",Toast.LENGTH_LONG).show();
+               }
+          }
+      });
     }
 
     @Override
--- app/src/main/java/android/dev/safesend/Text.java
+++ app/src/main/java/android/dev/safesend/Text.java
@@ -8,24 +8,42 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.gsm.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
-public class SMS extends Activity {
+public class Text extends Activity {
     Button btnSendSMS;
     EditText txtPhoneNo;
     EditText txtMessage;
     private String number;
     private String message;
-    android.telephony.SmsManager sms = new SmsManager();
-    public SMS(){
 
+    public Text(Contact c){
+        number = c.getNumber();
+        message = "I am OK";
+        Log.d("constructor","constructor");
+    //    sms = new SmsManager();
     }
-    public void sendSMS(){
-        sms.sendTextMessage (number,null, message, null, null);
-
+    public Text(Contact c, String mes){
+        number = c.getNumber();
+        message = mes;
+ //       sms= new SmsManager();
+    }
+    public void setNumber(String num){
+        number = num;
+    }
+    public void setMessage(String mes){
+        message = mes;
+    }
+    public void sendSMS() {
+        Log.d("textalmostsent","textalmostsent");
+        android.telephony.SmsManager sms = android.telephony.SmsManager.getDefault();
+        Log.d("textnearlysent","textnearlysent");
+        sms.sendTextMessage(number, null, message, null, null);
+        Log.d("textsent","textsent");
     }
 
 }
